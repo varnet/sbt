6ae69acd4b6daab411b1d2bb1b84fc935f7c6e80
varnet <vaux@ukr.net>
2017-10-25 19:34:04 +0300

    Create sbt.php

---
+++ sbt.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Simplified Benchmark Test by VR:2017 */
+
+$start = Timer();
+
+for ($i = 0; $i < 10000; $i++) {
+    for ($j = 0; $j < 10000; $j++) {
+        // any action
+    }
+}
+
+$stop = Timer();
+$trun = round($stop-$start,3);
+
+echo $trun."s\n";
+
+function Timer() {
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
+
+?>
